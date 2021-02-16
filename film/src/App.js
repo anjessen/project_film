950a4767c957bf8916cb061edbaf122810f59a93
anjessen <antoinejessen19@gmail.com>
2021-02-16 13:16:28 +0100

    refactorisation

--- film/src/App.js
+++ film/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Header, MovieList, MovieDetails, Loading, SearchBar } from './components';
-import apiMovie from './conf/api.movie';
+import { Header } from './components';
+import Films from './features/films';
 
 class App extends Component {
 
@@ -46,17 +46,13 @@ class App extends Component {
     return (
       <div className="App d-flex flex-column">
         <Header />
-        <SearchBar updateMovies= {this.updateMovies}/>
-        { this.state.loaded ? (
-          <div className="d-flex flex-row flex-fill pt-4 p-2" >
-            <MovieList 
-              movies={ this.state.movies } 
-              updateSelectedMovie={ this.updateSelectedMovie }/>
-            <MovieDetails movie={ this.state.movies[this.state.selectedMovie] }/>
-          </div>
-        ) : (
-          <Loading />
-        )}
+        <Films
+          loaded = { this.state.loaded }
+          updateMovies = { this.updateMovies }
+          updateSelectedMovie = { this.updateSelectedMovie }
+          movies = { this.state.movies }
+          selectedMovie = { this.state.selectedMovie }
+          />
       </div>
     );
   }
