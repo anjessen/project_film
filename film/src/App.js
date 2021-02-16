08ee62632e3b1641971069fb50c56ebe1d7010a3
anjessen <antoinejessen19@gmail.com>
2021-02-16 13:34:53 +0100

    refactorisation

--- film/src/App.js
+++ film/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Header } from './components';
 import Films from './features/films';
+import apiMovie, { apiMovieMap } from './conf/api.movie';
 
 class App extends Component {
 
@@ -18,12 +19,7 @@ class App extends Component {
     apiMovie.get('/discover/movie')
       .then( response => response.data.results )
       .then( moviesApi => {
-        const movies = moviesApi.map(m => ({ 
-          img: 'https://image.tmdb.org/t/p/w500' + m.poster_path,
-          title: m.title,
-          details: `${ m.release_date } | ${ m.vote_average }/10 (${ m.vote_count })`,
-          description: m.overview
-        }));
+        const movies = moviesApi.map(apiMovieMap);
         this.updateMovies(movies);
       })
       .catch( err => console.log(err));
